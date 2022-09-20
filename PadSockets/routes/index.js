ff89730c49143baa9b4a8e19c4953b12e078e0bb
Andy Gill <ZephrFish@users.noreply.github.com>
2022-09-20 21:16:54 +0100

    padd sockets addded

---
+++ PadSockets/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var shortid = require('shortid')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var sid = shortid.generate()
+  res.render('index', { title: 'Welcome to PadSockets', buttonLbl: 'Get started', id: sid })
+})
+
+module.exports = router
